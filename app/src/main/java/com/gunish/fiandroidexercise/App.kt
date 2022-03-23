66ca1e4497eb0bc7bf7c6cbc07c19bb4f00fe089
gunishmatta <gunishmatta@gmail.com>
2022-03-23 08:36:32 +0530

    base setup+made ui

---
+++ app/src/main/java/com/gunish/fiandroidexercise/App.kt
@@ -0,0 +1,7 @@
+package com.gunish.fiandroidexercise
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+open class App:Application()
---
+++ app/src/main/java/com/gunish/fiandroidexercise/utils/ViewExtenions.kt
@@ -0,0 +1,37 @@
+package com.gunish.fiandroidexercise.utils
+
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+fun View.toVisible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.toGone() {
+    this.visibility = View.GONE
+}
+
+fun View.toInvisible() {
+    this.visibility = View.GONE
+}
+
+fun View.showToast(
+    lifecycleOwner: LifecycleOwner,
+    ToastEvent: LiveData<SingleEvent<Any>>,
+    timeLength: Int
+) {
+
+    ToastEvent.observe(lifecycleOwner, Observer { event ->
+        event.getContentIfNotHandled()?.let {
+            when (it) {
+                is String -> Toast.makeText(this.context, it, timeLength).show()
+                is Int -> Toast.makeText(this.context, this.context.getString(it), timeLength).show()
+                else -> {
+                }
+            }
+        }
+    })
+}
